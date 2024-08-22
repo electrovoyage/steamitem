2f1cb82bd62e790cd1dc25fe66612bea9e8c5831
electrovoyage <75689188+TPEcool@users.noreply.github.com>
2024-08-22 13:05:15 +0300

    add button to re-open app link

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ function redirectToUrl() {
     const targetID = urlParams.get('id');
     if (targetID) {
         window.location.href = 'steam://openurl/https://steamcommunity.com/sharedfiles/filedetails/' + targetID
-        const content=document.querySelector('.status');
+        const content = document.querySelector('.status');
+        const button = document.querySelector('.button');
+        const button1 = document.querySelector('.button1');
         content.innerHTML = 'Check Steam! The page has been opened!'
+        button.style.display = 'inherit'
+        button1.style.display = 'inherit'
         return
     }
     //document.body.innerHTML = 'No URL provided.';
