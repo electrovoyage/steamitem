31817f85115eda176b9c2a34f636d7a4ca7e55cb
electrovoyage <75689188+TPEcool@users.noreply.github.com>
2024-08-22 12:48:14 +0300

    stylized page, added button to open in browser

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ function redirectToUrl() {
     const targetID = urlParams.get('id');
     if (targetID) {
         window.location.href = 'steam://openurl/https://steamcommunity.com/sharedfiles/filedetails/' + targetID
-        document.body.innerHTML = 'Check Steam! The page has been opened!'
+        const content=document.querySelector('.status');
+        content.innerHTML = 'Check Steam! The page has been opened!'
         return
     }
     //document.body.innerHTML = 'No URL provided.';
     window.location.href = "https://tpecool.github.io"
 }
+
+function opensteaminbrowser() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const targetID = urlParams.get('id');
+    if (targetID) {
+        window.location.href = 'https://steamcommunity.com/sharedfiles/filedetails/' + targetID
+    }
+}
+
 window.onload = redirectToUrl;
