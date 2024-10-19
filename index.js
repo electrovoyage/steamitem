dd179b71c50b365fd0b0f7b1833665751d26c0ff
electrovoyage <75689188+electrovoyage@users.noreply.github.com>
2024-10-19 18:56:34 +0300

    Migrate to centralized CDN

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ function redirectToUrl() {
     if (targetID) {
         window.location.href = 'steam://openurl/https://steamcommunity.com/sharedfiles/filedetails/' + targetID
         const content = document.querySelector('.status');
-        const button = document.querySelector('.button');
-        const button1 = document.querySelector('.button1');
+        const buttons = document.querySelector('.button');
         content.innerHTML = 'Check Steam! The page has been opened!'
-        button.style.display = 'inherit'
-        button1.style.display = 'inherit'
+        buttons.style.display = 'inherit'
         return
     }
     //document.body.innerHTML = 'No URL provided.';
