6646c186afd51dc0e2fe648fcc4aff74c1be1424
electrovoyage <75689188+electrovoyage@users.noreply.github.com>
2024-10-19 19:03:59 +0300

    Fix showing buttons

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ function redirectToUrl() {
     if (targetID) {
         window.location.href = 'steam://openurl/https://steamcommunity.com/sharedfiles/filedetails/' + targetID
         const content = document.querySelector('.status');
-        const buttons = document.querySelector('.button');
+        const buttons = document.querySelectorAll('.button');
         content.innerHTML = 'Check Steam! The page has been opened!'
-        buttons.style.display = 'inherit'
+        //buttons.style.display = 'inherit'
+
+        for (i = 0; i < buttons.length; i++) {
+            buttons[i].style.display = 'inherit'
+        }
+
         return
     }
     //document.body.innerHTML = 'No URL provided.';
