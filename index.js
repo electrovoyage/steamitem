57e047d3ac6fe76a7c0511f2e4372644e93389a7
electrovoyage <75689188+electrovoyage@users.noreply.github.com>
2024-10-26 15:16:43 +0300

    experimental font loading fix

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 function redirectToUrl() {
     const urlParams = new URLSearchParams(window.location.search);
     const targetID = urlParams.get('id');
-    if (targetID) {
-        window.location.href = 'steam://openurl/https://steamcommunity.com/sharedfiles/filedetails/' + targetID
-        const content = document.querySelector('.status');
-        const buttons = document.querySelectorAll('.button');
-        content.innerHTML = 'Check Steam! The page has been opened!'
-        //buttons.style.display = 'inherit'
 
-        for (i = 0; i < buttons.length; i++) {
-            buttons[i].style.display = 'inherit'
+    document.fonts.ready.then(() => {
+    
+        if (targetID) {
+            window.location.href = 'steam://openurl/https://steamcommunity.com/sharedfiles/filedetails/' + targetID
+            const content = document.querySelector('.status');
+            const buttons = document.querySelectorAll('.button');
+            content.innerHTML = 'Check Steam! The page has been opened!'
+            //buttons.style.display = 'inherit'
+
+            for (i = 0; i < buttons.length; i++) {
+                buttons[i].style.display = 'inherit'
+            }
+
+            return
         }
-
-        return
-    }
-    //document.body.innerHTML = 'No URL provided.';
-    window.location.href = "https://electrovoyage.github.io"
+        //document.body.innerHTML = 'No URL provided.';
+        window.location.href = "https://electrovoyage.github.io"
+    }).catch((error) => {
+        console.error(error)
+    });
 }
+
+
+/*
+document.fonts.ready.then(() => {
+    console.log('All fonts are loaded');
+    // You can now use the loaded fonts
+}).catch((error) => {
+    console.error(error);
+});
+*/
 
 function opensteaminbrowser() {
     const urlParams = new URLSearchParams(window.location.search);
